b010101f70111155f0600576b7472c92d4e99d31
RiffXS <rafaelbleaob@gmail.com>
2025-05-29 14:50:17 -0300

    Adicionando separação para testes de estilização e comportamento

--- src/vitest/GenericDatePicker.test.ts
+++ src/vitest/GenericDatePicker.test.ts
@@ -33,134 +33,137 @@ const inputStyleMap: Record<inputState, {
 }
 
 describe('GenericDatePicker.vue', () => {
-  it.each(
-    inputStates.flatMap(state =>
-      requiredValues.map(required => [state, required] as [inputState, boolean])
+  describe('testes de estilização', () => {
+    it.each(
+      inputStates.flatMap(state =>
+        requiredValues.map(required => [state, required] as [inputState, boolean])
+      )
+    )(
+      'renderiza corretamente com state "%s" e required %s',
+      (state, required) => {
+        const { getByLabelText, getByText } = render(GenericDatePicker, {
+          props: {
+            label: `DatePicker ${state} ${required ? 'required' : 'optional'}`,
+            placeholder: `${state} ${required}`,
+            state,
+            required
+          }
+        })
+
+        const label = getByText(`DatePicker ${state} ${required ? 'required' : 'optional'}`, { exact: false })
+        const datePicker = getByLabelText(`DatePicker ${state} ${required ? 'required' : 'optional'}`, { exact: false })
+
+        // Verifica a aplicação do anel
+        expect(datePicker).toHaveClass(`${inputStyleMap[state].ring}`)
+
+        // Verifica a aplicação da cor do background
+        if (inputStyleMap[state].bg) {
+          expect(datePicker).toHaveClass(`${inputStyleMap[state].bg}`)
+        }
+
+        // Verifica a aplicação do placeholder no DatePicker
+        expect(datePicker).toHaveAttribute('placeholder', `${state} ${required}`)
+
+        // Verifica a aplicação da label no DatePicker
+        expect(label).toBeTruthy()
+
+        // Verifica a aplicação do * quando required
+        if (required) {
+          expect(label).toHaveTextContent(/\*/)
+        } else {
+          expect(label).not.toHaveTextContent(/\*/)
+        }
+      }
     )
-  )(
-    'renderiza corretamente com state "%s" e required %s',
-    (state, required) => {
-      const { getByLabelText, getByText } = render(GenericDatePicker, {
-        props: {
-          label: `DatePicker ${state} ${required ? 'required' : 'optional'}`,
-          placeholder: `${state} ${required}`,
-          state,
-          required
-        }
-      })
-
-      const label = getByText(`DatePicker ${state} ${required ? 'required' : 'optional'}`, { exact: false })
-      const datePicker = getByLabelText(`DatePicker ${state} ${required ? 'required' : 'optional'}`, { exact: false })
-
-      // Verifica a aplicação do anel
-      expect(datePicker).toHaveClass(`${inputStyleMap[state].ring}`)
-
-      // Verifica a aplicação da cor do background
-      if (inputStyleMap[state].bg) {
-        expect(datePicker).toHaveClass(`${inputStyleMap[state].bg}`)
-      }
-
-      // Verifica a aplicação do placeholder no DatePicker
-      expect(datePicker).toHaveAttribute('placeholder', `${state} ${required}`)
-
-      // Verifica a aplicação da label no DatePicker
-      expect(label).toBeTruthy()
-
-      // Verifica a aplicação do * quando required
-      if (required) {
-        expect(label).toHaveTextContent(/\*/)
-      } else {
-        expect(label).not.toHaveTextContent(/\*/)
-      }
-    }
-  )
-
-  it('vincula corretamente o valor com v-model', async () => {
-    const modelValue = ref<string | undefined>('2024-12-31')
-
-    const { getByDisplayValue } = render(GenericDatePicker, {
-      props: {
-        label: 'Data',
-        placeholder: 'Escolha',
-        modelValue: modelValue.value,
-        'onUpdate:modelValue': (val: string | undefined) => {
-          modelValue.value = val
-        }
-      }
-    })
-
-    const input = getByDisplayValue('2024-12-31') as HTMLInputElement
-    expect(input.value).toBe('2024-12-31')
-
-    await fireEvent.update(input, '2025-01-01')
-    expect(modelValue.value).toBe('2025-01-01')
   })
 
-  it('envio de formulário', async () => {
-    const onSubmit = vi.fn()
-    const { getByLabelText, getByText } = render({
-      components: { GenericDatePicker, GenericButton },
-      template: `
-        <form @submit="submitForm">
-          <GenericDatePicker v-model="inputModel" label="DatePicker" placeholder="Escolha" />
-          <GenericButton label="Enviar" type="submit" />
-        </form>
-      `,
-      setup() {
-        const inputModel = ref()
-        const submitForm = () => {
-          onSubmit(inputModel.value)
-        }
-        return { inputModel, submitForm }
-      }
-    })
+  describe('testes de comportamento', () => {
+    it('vincula corretamente o valor com v-model', async () => {
+      const modelValue = ref<string | undefined>('2024-12-31')
 
-    const datePicker = getByLabelText('DatePicker') as HTMLInputElement
-    const button = getByText('Enviar')
-
-    expect(datePicker).toHaveTextContent('')
-    await fireEvent.update(datePicker, '2022-12-13')
-    fireEvent.click(button)
-    expect(onSubmit).toHaveBeenCalledWith('2022-12-13')
-  })
-
-  it.each(inputStates.filter(s => s !== 'disabled'))(
-    'permite interação quando state = "%s"',
-    async (state) => {
-      const modelValue = ref<string | undefined>('')
-
-      const { getByPlaceholderText } = render(GenericDatePicker, {
+      const { getByLabelText } = render(GenericDatePicker, {
         props: {
-          label: 'Data',
-          placeholder: 'Digite...',
-          state,
+          label: 'DatePicker',
+          placeholder: 'Escolha',
           modelValue: modelValue.value,
-          'onUpdate:modelValue': (val: string | undefined) => (modelValue.value = val)
+          'onUpdate:modelValue': (val: string | undefined) => {
+            modelValue.value = val
+          }
         }
       })
 
-      const input = getByPlaceholderText('Digite...') as HTMLInputElement
-      expect(input.disabled).toBe(false)
+      const input = getByLabelText('DatePicker') as HTMLInputElement
+      expect(input).toHaveValue('2024-12-31')
 
-      await fireEvent.update(input, '2025-05-21')
-      expect(modelValue.value).toBe('2025-05-21')
-    }
-  )
-
-  it('exibe mensagens de erro corretamente', () => {
-    const { getByText, getByPlaceholderText } = render(GenericDatePicker, {
-      props: {
-        label: 'Data de início',
-        placeholder: 'Selecionar data',
-        errorMessages: ['Campo obrigatório', 'Data inválida']
-      }
+      await fireEvent.update(input, '2025-01-01')
+      expect(modelValue.value).toBe('2025-01-01')
     })
 
-    expect(getByText('Campo obrigatório')).toBeInTheDocument()
-    expect(getByText('Data inválida')).toBeInTheDocument()
+    it('envio de formulário', async () => {
+      const onSubmit = vi.fn()
+      const { getByLabelText, getByText } = render({
+        components: { GenericDatePicker, GenericButton },
+        template: `
+          <form @submit="submitForm">
+            <GenericDatePicker v-model="inputModel" label="DatePicker" placeholder="Escolha" />
+            <GenericButton label="Enviar" type="submit" />
+          </form>
+        `,
+        setup() {
+          const inputModel = ref()
+          const submitForm = () => {
+            onSubmit(inputModel.value)
+          }
+          return { inputModel, submitForm }
+        }
+      })
 
-    const input = getByPlaceholderText('Selecionar data') as HTMLInputElement
-    expect(input.classList.contains('bg-error-100/10')).toBe(true)
+      const datePicker = getByLabelText('DatePicker') as HTMLInputElement
+      const button = getByText('Enviar')
+
+      await fireEvent.update(datePicker, '2022-12-13')
+      fireEvent.click(button)
+      expect(onSubmit).toHaveBeenCalledWith('2022-12-13')
+    })
+
+    it.each(inputStates.filter(s => s !== 'disabled'))(
+      'permite interação quando state = "%s"',
+      async (state) => {
+        const modelValue = ref<string | undefined>('')
+
+        const { getByPlaceholderText } = render(GenericDatePicker, {
+          props: {
+            label: 'Data',
+            placeholder: 'Digite...',
+            state,
+            modelValue: modelValue.value,
+            'onUpdate:modelValue': (val: string | undefined) => (modelValue.value = val)
+          }
+        })
+
+        const input = getByPlaceholderText('Digite...') as HTMLInputElement
+        expect(input.disabled).toBe(false)
+
+        await fireEvent.update(input, '2025-05-21')
+        expect(modelValue.value).toBe('2025-05-21')
+      }
+    )
+
+    it('exibe mensagens de erro corretamente', () => {
+      const { getByText, getByPlaceholderText } = render(GenericDatePicker, {
+        props: {
+          label: 'Data de início',
+          placeholder: 'Selecionar data',
+          errorMessages: ['Campo obrigatório', 'Data inválida']
+        }
+      })
+
+      expect(getByText('Campo obrigatório')).toBeInTheDocument()
+      expect(getByText('Data inválida')).toBeInTheDocument()
+
+      const input = getByPlaceholderText('Selecionar data') as HTMLInputElement
+      expect(input.classList.contains('bg-error-100/10')).toBe(true)
+    })
   })
 
 })
--- src/vitest/GenericSelect.test.ts
+++ src/vitest/GenericSelect.test.ts
@@ -34,113 +34,60 @@ const selectStyleMap: Record<selectState, {
 }
 
 describe('GenericSelect.vue', () => {
-  it.each(
-    selectStates.flatMap(state =>
-      requiredValues.map(required => [state, required] as [selectState, boolean])
+  describe('testes de estilização', () => {
+    it.each(
+      selectStates.flatMap(state =>
+        requiredValues.map(required => [state, required] as [selectState, boolean])
+      )
+    )(
+      'renderiza corretamente com state "%s"',
+      (state, required) => {
+        const { getByLabelText, getByText } = render(GenericSelect, {
+          props: {
+            label: `Select ${state} ${required ? 'required' : 'optional'}`,
+            placeholder: `${state} ${required}`,
+            options: [],
+            state,
+            required
+          }
+        })
+
+        const label = getByText(`Select ${state} ${required ? 'required' : 'optional'}`, { exact: false })
+        const select = getByLabelText(`Select ${state} ${required ? 'required' : 'optional'}`, { exact: false })
+
+        // Verifica a aplicação do anel
+        expect(select).toHaveClass(`${selectStyleMap[state].ring}`)
+
+        // Verifica a aplicação da cor do background
+        if (selectStyleMap[state].bg) {
+          expect(select).toHaveClass(`${selectStyleMap[state].bg}`)
+        }
+
+        // Verifica a aplicação do placeholder no Select
+        expect(select.firstChild).toHaveTextContent(`${state} ${required}`)
+      
+        // Verifica a aplicação de label no Select
+        expect(label).toBeTruthy()
+
+        // Verifica a aplicação de * quando required
+        if (required) {
+          expect(label).toHaveTextContent(/\*/)
+        } else {
+          expect(label).not.toHaveTextContent(/\*/)
+        }
+      }
     )
-  )(
-    'renderiza corretamente com state "%s"',
-    (state, required) => {
-      const { getByLabelText, getByText } = render(GenericSelect, {
-        props: {
-          label: `Select ${state} ${required ? 'required' : 'optional'}`,
-          placeholder: `${state} ${required}`,
-          options: [],
-          state,
-          required
-        }
-      })
-
-      const label = getByText(`Select ${state} ${required ? 'required' : 'optional'}`, { exact: false })
-      const select = getByLabelText(`Select ${state} ${required ? 'required' : 'optional'}`, { exact: false })
-
-      // Verifica a aplicação do anel
-      expect(select).toHaveClass(`${selectStyleMap[state].ring}`)
-
-      // Verifica a aplicação da cor do background
-      if (selectStyleMap[state].bg) {
-        expect(select).toHaveClass(`${selectStyleMap[state].bg}`)
-      }
-
-      // Verifica a aplicação do placeholder no Select
-      expect(select.firstChild).toHaveTextContent(`${state} ${required}`)
-    
-      // Verifica a aplicação de label no Select
-      expect(label).toBeTruthy()
-
-      // Verifica a aplicação de * quando required
-      if (required) {
-        expect(label).toHaveTextContent(/\*/)
-      } else {
-        expect(label).not.toHaveTextContent(/\*/)
-      }
-    }
-  )
-
-  it('vincula corretamente o valor com v-model', async () => {
-    const model = ref<string | number | undefined>('banana')
-
-    const { getByDisplayValue } = render(GenericSelect, {
-      props: {
-        label: 'Fruta',
-        placeholder: 'Selecione',
-        options: ['banana', 'maçã', 'uva'],
-        modelValue: model.value,
-        'onUpdate:modelValue': (val: string | number | undefined) => {
-          model.value = val
-        }
-      }
-    })
-
-    const select = getByDisplayValue('banana') as HTMLSelectElement
-    expect(select.value).toBe('banana')
-
-    await fireEvent.update(select, 'uva')
-    expect(model.value).toBe('uva')
   })
 
-  it('envio de formulário', () => {
-    const onSubmit = vi.fn()
-    const { getByLabelText, getByText } = render({
-      components: { GenericSelect, GenericButton },
-      template: `
-        <form @submit="submitForm">
-          <GenericSelect v-model="selectModel" label="Select" placeholder="Testando" :options="['Teste', '123']" />
-          <GenericButton label="Enviar" type="submit" />
-        </form>
-      `,
-      setup() {
-        const selectModel = ref()
-        const submitForm = () => {
-          onSubmit(selectModel.value)
-        }
-        return { selectModel, submitForm }
-      }
-    })
-
-    const select = getByLabelText('Select') as HTMLSelectElement
-    const button = getByText('Enviar')
-
-    fireEvent.change(select, { target: { value: '123' } })
-    fireEvent.click(button)
-    expect(onSubmit).toHaveBeenCalledWith('123')
-
-    fireEvent.change(select, { target: { value: 'Algo' } })
-    fireEvent.click(button)
-    expect(onSubmit).toHaveBeenCalledWith(undefined)
-  })
-
-  it.each(selectStates.filter(s => s !== 'disabled'))(
-    'permite interação quando state = "%s"',
-    async (state) => {
-      const model = ref<string | number | undefined>('')
+  describe('testes de comportamento', () => {
+    it('vincula corretamente o valor com v-model', async () => {
+      const model = ref<string | number | undefined>('banana')
 
       const { getByDisplayValue } = render(GenericSelect, {
         props: {
-          label: 'Estado',
-          placeholder: 'Selecionar',
-          options: ['ativo', 'inativo'],
-          state,
+          label: 'Fruta',
+          placeholder: 'Selecione',
+          options: ['banana', 'maçã', 'uva'],
           modelValue: model.value,
           'onUpdate:modelValue': (val: string | number | undefined) => {
             model.value = val
@@ -148,42 +95,99 @@ describe('GenericSelect.vue', () => {
         }
       })
 
+      const select = getByDisplayValue('banana')
+      expect(select).toHaveValue('banana')
+
+      await fireEvent.update(select, 'uva')
+      expect(model.value).toBe('uva')
+    })
+
+    it('envio de formulário', () => {
+      const onSubmit = vi.fn()
+      const { getByLabelText, getByText } = render({
+        components: { GenericSelect, GenericButton },
+        template: `
+          <form @submit="submitForm">
+            <GenericSelect v-model="selectModel" label="Select" placeholder="Testando" :options="['Teste', '123']" />
+            <GenericButton label="Enviar" type="submit" />
+          </form>
+        `,
+        setup() {
+          const selectModel = ref()
+          const submitForm = () => {
+            onSubmit(selectModel.value)
+          }
+          return { selectModel, submitForm }
+        }
+      })
+
+      const select = getByLabelText('Select') as HTMLSelectElement
+      const button = getByText('Enviar')
+
+      fireEvent.update(select, '123')
+      fireEvent.click(button)
+      expect(onSubmit).toHaveBeenCalledWith('123')
+
+      fireEvent.update(select, 'Algo')
+      fireEvent.click(button)
+      expect(onSubmit).toHaveBeenCalledWith(undefined)
+    })
+
+    it.each(selectStates.filter(s => s !== 'disabled'))(
+      'permite interação quando state = "%s"',
+      async (state) => {
+        const model = ref<string | number | undefined>('')
+
+        const { getByDisplayValue } = render(GenericSelect, {
+          props: {
+            label: 'Estado',
+            placeholder: 'Selecionar',
+            options: ['ativo', 'inativo'],
+            state,
+            modelValue: model.value,
+            'onUpdate:modelValue': (val: string | number | undefined) => {
+              model.value = val
+            }
+          }
+        })
+
+        const select = getByDisplayValue('Selecionar') as HTMLSelectElement
+        expect(select).toBeEnabled()
+
+        await fireEvent.update(select, 'ativo')
+        expect(model.value).toBe('ativo')
+      }
+    )
+
+    it('desabilita o select se state for "disabled"', async () => {
+      const { getByDisplayValue } = render(GenericSelect, {
+        props: {
+          label: 'Estado',
+          placeholder: 'Selecionar',
+          options: ['ativo', 'inativo'],
+          state: 'disabled'
+        }
+      })
+
       const select = getByDisplayValue('Selecionar') as HTMLSelectElement
-      expect(select.disabled).toBe(false)
-
-      await fireEvent.update(select, 'ativo')
-      expect(model.value).toBe('ativo')
-    }
-  )
-
-  it('desabilita o select se state for "disabled"', () => {
-    const { getByDisplayValue } = render(GenericSelect, {
-      props: {
-        label: 'Estado',
-        placeholder: 'Selecionar',
-        options: ['ativo', 'inativo'],
-        state: 'disabled'
-      }
+      expect(select).toBeDisabled()
     })
 
-    const select = getByDisplayValue('Selecionar') as HTMLSelectElement
-    expect(select.disabled).toBe(true)
-  })
+    it('exibe mensagens de erro corretamente e aplica classe de erro', () => {
+      const { getByText, getByDisplayValue } = render(GenericSelect, {
+        props: {
+          label: 'Curso',
+          placeholder: 'Escolha',
+          options: ['Engenharia', 'Medicina'],
+          errorMessages: ['Campo obrigatório', 'Valor inválido']
+        }
+      })
 
-  it('exibe mensagens de erro corretamente e aplica classe de erro', () => {
-    const { getByText, getByDisplayValue } = render(GenericSelect, {
-      props: {
-        label: 'Curso',
-        placeholder: 'Escolha',
-        options: ['Engenharia', 'Medicina'],
-        errorMessages: ['Campo obrigatório', 'Valor inválido']
-      }
+      expect(getByText('Campo obrigatório')).toBeInTheDocument()
+      expect(getByText('Valor inválido')).toBeInTheDocument()
+
+      const select = getByDisplayValue('Escolha') as HTMLSelectElement
+      expect(select).toHaveClass('bg-error-100/10')
     })
-
-    expect(getByText('Campo obrigatório')).toBeInTheDocument()
-    expect(getByText('Valor inválido')).toBeInTheDocument()
-
-    const select = getByDisplayValue('Escolha') as HTMLSelectElement
-    expect(select).toHaveClass('bg-error-100/10')
   })
 })
