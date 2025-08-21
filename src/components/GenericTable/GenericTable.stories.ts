942d1c50a7708063cd94a8c5ff2e538fe3c88198
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2025-08-21 13:18:53 -0300

    [REFACTOR] Atualizado storybook GenericTable

--- src/components/GenericTable/GenericTable.stories.ts
+++ src/components/GenericTable/GenericTable.stories.ts
@@ -2,45 +2,60 @@ import type { Meta, StoryObj } from '@storybook/vue3';
 
 import GenericTable from './GenericTable.vue';
 
-const meta = {
-  title: 'Basic Components/GenericTable',
+const meta: Meta<typeof GenericTable> = {
+  title: 'Components/GenericTable',
   component: GenericTable,
   tags: ['autodocs'],
 
-  argTypes: {
-    headers: { control: 'object', description: 'Configurações da coluna da tabela', required: true },
-    items: { control: 'object', description: 'Itens da tabela', required: true },
-    totalRecords: { control: 'number', description: 'Quantidade total de registros' },
-    itemsPerPage: { control: 'number', description: 'Quantidade de registros por página', required: true },
-    totalPages: { control: 'number', description: 'Quantidade total de páginas' },
-    page: { control: 'number', description: 'Página atual' },
+  parameters: {
+    docs: {
+      description: {
+        component: 'Tabela',
+      },
+      extractArgTypes: false,
+      extractComponentDescription: false,
+    }
   },
 
-} satisfies Meta<typeof GenericTable>;
+  argTypes: {
+    columns: {
+      control: 'object',
+      description: 'Configurações da coluna da tabela',
+      required: true
+    },
+    data: {
+      control: 'object',
+      description: 'Itens da tabela',
+      required: true
+    },
+    itemsPerPage: {
+      control: 'number',
+      description: 'Quantidade de registros por página. Se presente, ativa a paginação.',
+      required: false
+    },
+  },
+
+}
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+
+type Story = StoryObj<typeof GenericTable>;
 
 export const Default: Story = {
   args: {
-    headers: {
-        name: {title: 'Nome', type: 'text', sortable: true},
-        link: {title: 'Link', type: 'link', sortable: true},
-        date: {title: 'Data', type: 'date', sortable: true},
-        status: {title: 'Status', type: 'status', sortable: true},
-        currency: {title: 'Dinheiro', type: 'currency', sortable: true},
-        actions: {title: 'Ações', type: 'actions'},
-    },
-    items: [
-        {name: 'Nome 1', link: 'Link 1', date: '2025-05-07T16:47:56.550Z', status: 'Ativo', currency: 1000, actions: ['delete']},
-        {name: 'Nome 2', link: 'Link 2', date: '2026-02-28T16:47:56.550Z', status: 'Inativo', currency: 2000, actions: ['view', 'edit', 'delete']},
-        {name: 'Nome 3', link: 'Link 3', date: '2023-07-10T16:47:56.550Z', status: 'Ativo', currency: 2000, actions: ['view', 'edit', 'delete']},
-        {name: 'Nome 4', link: 'Link 4', date: '2026-03-17T16:47:56.550Z', status: 'Ativo', currency: 7000, actions: ['view', 'edit']},
-        {name: 'Nome 5', link: 'Link 5', date: '2025-02-15T16:47:56.550Z', status: 'Ativo', currency: 400, actions: ['view']},
+    columns: [
+        { key: 'name', title: 'Nome', sortable: true},
+        { key: 'link', title: 'Link', sortable: true},
+        { key: 'date', title: 'Data', sortable: true},
+        { key: 'status', title: 'Status', sortable: true},
+        { key: 'currency', title: 'Dinheiro', sortable: true},
+    ],
+    data: [
+        { name: 'Nome 1', link: 'Link 1', date: '2025-05-07T16:47:56.550Z', status: 'Ativo', currency: 1000, actions: ['delete']},
+        { name: 'Nome 2', link: 'Link 2', date: '2026-02-28T16:47:56.550Z', status: 'Inativo', currency: 2000, actions: ['view', 'edit', 'delete']},
+        { name: 'Nome 3', link: 'Link 3', date: '2023-07-10T16:47:56.550Z', status: 'Ativo', currency: 2000, actions: ['view', 'edit', 'delete']},
+        { name: 'Nome 4', link: 'Link 4', date: '2026-03-17T16:47:56.550Z', status: 'Ativo', currency: 7000, actions: ['view', 'edit']},
+        { name: 'Nome 5', link: 'Link 5', date: '2025-02-15T16:47:56.550Z', status: 'Ativo', currency: 400, actions: ['view']},
       ],
-      totalRecords: 25,
-      itemsPerPage: 5,
-      totalPages: 5,
-      page: 1,
     }
 }
