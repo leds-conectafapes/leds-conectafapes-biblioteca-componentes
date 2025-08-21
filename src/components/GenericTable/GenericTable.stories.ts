1b449b7c128adb41484e60fc7719a8538919d578
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2025-08-21 15:29:05 -0300

    [FEAT] adicionadas informacoes no Storybook de GenericTable

--- src/components/GenericTable/GenericTable.stories.ts
+++ src/components/GenericTable/GenericTable.stories.ts
@@ -4,6 +4,7 @@ import GenericTable from './GenericTable.vue';
 
 const meta: Meta<typeof GenericTable> = {
   title: 'Components/GenericTable',
+  // https://github.com/storybookjs/storybook/issues/24238
   // @ts-ignore
   component: GenericTable,
   tags: ['autodocs'],
@@ -11,7 +12,53 @@ const meta: Meta<typeof GenericTable> = {
   parameters: {
     docs: {
       description: {
-        component: 'Tabela',
+        component: `
+**GenericTable** é um componente de tabela tipado com suporte a genéricos e slots.
+
+- Suporta configuração de colunas por meio de *array* de objetos
+- Suporta dados das linhas por meio de *array* de objetos
+- Agiliza implementação de funcionalidades costumeiras por meio de coluna de ações
+- Suporta paginação *server-side*
+- Oferece controle fino de exibição de colunas, linhas e até células especificas por meio de *slots* e *render functions*
+
+### Exemplo:
+\`\`\`vue
+<script setup lang="ts">
+import { GenericTable } from '@leds-ifes/components'
+import type { TableHeader } from '@leds-ifes/components'
+
+type Data = {
+  name: string
+  link: string
+  date: string,
+  status 'Ativo' | 'Inativo'
+  currency: number
+}
+
+const columns: TableHeader<Data> = [
+    { key: 'name', title: 'Nome', sortable: true },
+    { key: 'link', title: 'Link', sortable: true },
+    { key: 'date', title: 'Data', sortable: true },
+    { key: 'status', title: 'Status', sortable: true },
+    { key: 'currency', title: 'Dinheiro', sortable: true },
+]
+
+const data: Data[] = [
+  { name: 'Nome 1', link: 'Link 1', date: '2025-05-07', status: 'Ativo', currency: 1000 },
+  { name: 'Nome 2', link: 'Link 2', date: '2026-02-28', status: 'Inativo', currency: 2000 },
+  { name: 'Nome 3', link: 'Link 3', date: '2023-07-10', status: 'Ativo', currency: 2000 },
+  { name: 'Nome 4', link: 'Link 4', date: '2026-03-17', status: 'Ativo', currency: 7000 },
+  { name: 'Nome 5', link: 'Link 5', date: '2025-02-15', status: 'Ativo', currency: 400 },
+]
+</script>
+<template>
+  <GenericTable
+    :columns="columns"
+    :data="data"
+  />
+</template>
+\`\`\`
+        `.trim(),
       },
       extractArgTypes: false,
       extractComponentDescription: false,
@@ -29,11 +76,30 @@ const meta: Meta<typeof GenericTable> = {
       description: 'Itens da tabela',
       required: true
     },
+    page: {
+      control: 'number',
+      description: 'Página em exibição.'
+    },
     itemsPerPage: {
       control: 'number',
       description: 'Quantidade de registros por página. Se presente, ativa a paginação.',
-      required: false
     },
+    action: {
+      control: 'object',
+      description: 'Configurações da coluna de ações. A coluna de ações apresenta botões para ações rotineiras como visualização, edição e exclusão de dados. Se presente, coluna de ações é exibida.'
+    },
+    loading: {
+      control: 'boolean',
+      description: 'Aponta se os dados da tabela estão sendo carregados ou não.'
+    },
+    emptyText: {
+      control: 'string',
+      description: 'Texto exibido quando os dados estão carregados, mas vazios.',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: `'Nenhum resultado encontrado'` }
+      }
+    }
   },
 
 }
@@ -52,11 +118,11 @@ export const Default: Story = {
         { key: 'currency', title: 'Dinheiro', sortable: true},
     ],
     data: [
-        { name: 'Nome 1', link: 'Link 1', date: '2025-05-07T16:47:56.550Z', status: 'Ativo', currency: 1000, actions: ['delete']},
-        { name: 'Nome 2', link: 'Link 2', date: '2026-02-28T16:47:56.550Z', status: 'Inativo', currency: 2000, actions: ['view', 'edit', 'delete']},
-        { name: 'Nome 3', link: 'Link 3', date: '2023-07-10T16:47:56.550Z', status: 'Ativo', currency: 2000, actions: ['view', 'edit', 'delete']},
-        { name: 'Nome 4', link: 'Link 4', date: '2026-03-17T16:47:56.550Z', status: 'Ativo', currency: 7000, actions: ['view', 'edit']},
-        { name: 'Nome 5', link: 'Link 5', date: '2025-02-15T16:47:56.550Z', status: 'Ativo', currency: 400, actions: ['view']},
+        { name: 'Nome 1', link: 'Link 1', date: '2025-05-07', status: 'Ativo', currency: 1000 },
+        { name: 'Nome 2', link: 'Link 2', date: '2026-02-28', status: 'Inativo', currency: 2000 },
+        { name: 'Nome 3', link: 'Link 3', date: '2023-07-10', status: 'Ativo', currency: 2000 },
+        { name: 'Nome 4', link: 'Link 4', date: '2026-03-17', status: 'Ativo', currency: 7000 },
+        { name: 'Nome 5', link: 'Link 5', date: '2025-02-15', status: 'Ativo', currency: 400 },
       ],
     }
 }
