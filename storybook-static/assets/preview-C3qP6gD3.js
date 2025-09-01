b1505d945bcf977c039959e1306750705373388e
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2025-09-01 09:11:26 -0300

    remove storybook-static directory

--- storybook-static/assets/preview-C3qP6gD3.js
+++
@@ -1,8 +0,0 @@
-import { _ as e } from "./index--3KtMZOq.js";
-const r = {
-  parameters: {
-    docs: { theme: e.dark },
-    controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } },
-  },
-};
-export { r as default };
