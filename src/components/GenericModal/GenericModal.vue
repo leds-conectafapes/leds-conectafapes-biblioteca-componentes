464300f2e9ece743a6814a94cc68cf5ecad2fd30
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2025-09-15 17:12:01 -0300

    [FIX] alinhamento do botao de fechar de GenericModal

--- src/components/GenericModal/GenericModal.vue
+++ src/components/GenericModal/GenericModal.vue
@@ -60,7 +60,7 @@ const cardClass = computed(() => {
 
 <template>
   <div :class="overlayClass">
-    <div :class="cardClass">
+    <div v-bind="attrs" :class="cardClass">
       <div class="flex items-center gap-2">
         <span class="text-xl leading-normal font-bold text-gray-800 grow h-fit">
           {{ title }}
@@ -69,7 +69,7 @@ const cardClass = computed(() => {
         <GenericIcon
           v-if="!hideCloseIcon"
           name="close"
-          class="cursor-pointer text-xl leading-normal font-bold text-gray-800"
+          class="cursor-pointer text-xl leading-normal font-bold text-gray-800 self-start"
           :weight="400"
           @click="close"
         />
