75813e5ed9773ea2b212d5c5c336a4fefbb53917
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2025-09-29 13:35:00 -0300

    [FIX] alinhamento do check de GenericCheckBox

--- src/components/GenericCheckbox/GenericCheckbox.vue
+++ src/components/GenericCheckbox/GenericCheckbox.vue
@@ -6,6 +6,7 @@
 import { computed, useAttrs, useSlots } from "vue";
 import { cn } from "../../utils/cn";
 import type { InputHTMLAttributes } from "vue";
+import GenericIcon from "../GenericIcon/GenericIcon.vue";
 
 defineOptions({ inheritAttrs: false });
 
@@ -31,7 +32,7 @@ const hasLabelSlot = computed(() => !!slots.label);
 
 const checkboxClass = computed(() =>
   cn(
-    "w-5 h-5 appearance-none rounded-xs border-1 border-gray-500 cursor-pointer checked:border-0 checked:bg-primary-500",
+    "w-5 h-5 appearance-none rounded-xs border-1 border-gray-500 cursor-pointer checked:border-0 checked:bg-primary-500 block",
     attrs.class as string | undefined,
   ),
 );
@@ -46,17 +47,25 @@ const forwarded = computed(() => {
   <div
     :class="
       cn(
-        'group relative flex flex-row items-center gap-3 w-fit cursor-pointer select-none font-inter',
+        'flex flex-row items-center gap-3 w-fit cursor-pointer select-none font-inter',
         props.containerClass,
       )
     "
   >
-    <input
-      v-bind="forwarded"
-      v-model="modelValue"
-      type="checkbox"
-      :class="checkboxClass"
-    />
+    <div class="group relative">
+      <input
+        v-bind="forwarded"
+        v-model="modelValue"
+        type="checkbox"
+        :class="checkboxClass"
+      />
+
+      <GenericIcon
+        name="check"
+        class="pointer-events-none absolute top-[-1px] left-[-2px] hidden group-has-checked:block leading-none text-2xl text-white"
+        variant="rounded"
+      />
+    </div>
 
     <label v-if="!hasLabelSlot" :for="id">
       {{ label }}
@@ -64,19 +73,5 @@ const forwarded = computed(() => {
     <label v-else-if="hasLabelSlot" :for="id">
       <slot name="label" />
     </label>
-
-    <svg
-      pointer-events="none"
-      width="16"
-      height="13"
-      viewBox="0 0 16 13"
-      class="absolute left-1 size-3.5 hidden group-has-checked:block"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M4.75025 10.0402L1.56941 6.8594C1.21191 6.5019 0.634414 6.5019 0.276914 6.8594C-0.0805859 7.2169 -0.0805859 7.7944 0.276914 8.1519L4.10858 11.9836C4.46608 12.3411 5.04358 12.3411 5.40108 11.9836L15.0994 2.28523C15.4569 1.92773 15.4569 1.35023 15.0994 0.992734C14.7419 0.635234 14.1644 0.635234 13.8069 0.992734L4.75025 10.0402Z"
-        fill="white"
-      />
-    </svg>
   </div>
 </template>
