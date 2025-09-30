8766bbdbb4bf9c3f729d3e14dc13f0c8d9634998
Lucas Vieira <lucasvieiradasilva07@gmail.com>
2025-09-30 16:15:34 +0000

    [FIX] alinhamento do check do GenericCheckbox (#74)
    * [FIX] alinhamento do check de GenericCheckBox
    
    * [REFACTOR] mudado slot para nao exibir label quando nao informado
    
    * [FEAT] add variacao de cores quando desabilitado em GenericCheckbox
    
    * [FIX] removidas classes cursor-pointer select-none d GenericCheckbox
    
    * [FIX] tirado console.log
    
    * [FIX] add select-none ao elemento conteiner do checkbox

--- src/components/GenericCheckbox/GenericCheckbox.vue
+++ src/components/GenericCheckbox/GenericCheckbox.vue
@@ -3,9 +3,10 @@
   setup
   generic="T extends string | number | boolean | undefined"
 >
-import { computed, useAttrs, useSlots } from "vue";
+import { computed, useAttrs } from "vue";
 import { cn } from "../../utils/cn";
 import type { InputHTMLAttributes } from "vue";
+import GenericIcon from "../GenericIcon/GenericIcon.vue";
 
 defineOptions({ inheritAttrs: false });
 
@@ -14,24 +15,26 @@ type NativeCheckboxAttributes = /* @vue-ignore */ InputHTMLAttributes;
 type CheckboxProps = {
   label?: string;
   containerClass?: string | string[];
+  disabled?: boolean;
 } & NativeCheckboxAttributes;
 
 const props = withDefaults(defineProps<CheckboxProps>(), {
   label: "",
   containerClass: () => [],
+  disabled: false,
 });
 
 const modelValue = defineModel<T>();
 
-const slots = useSlots();
 const attrs = useAttrs();
 const id = computed(() => attrs.id as string | undefined);
 
-const hasLabelSlot = computed(() => !!slots.label);
-
 const checkboxClass = computed(() =>
   cn(
-    "w-5 h-5 appearance-none rounded-xs border-1 border-gray-500 cursor-pointer checked:border-0 checked:bg-primary-500",
+    "w-5 h-5 appearance-none rounded-xs border-1 border-gray-500 cursor-pointer  block",
+    props.disabled
+      ? "bg-gray-100 checked:bg-gray-500"
+      : "checked:border-0 checked:bg-primary-500",
     attrs.class as string | undefined,
   ),
 );
@@ -46,37 +49,31 @@ const forwarded = computed(() => {
   <div
     :class="
       cn(
-        'group relative flex flex-row items-center gap-3 w-fit cursor-pointer select-none font-inter',
+        'flex flex-row items-center gap-3 w-fit font-inter',
         props.containerClass,
       )
     "
   >
-    <input
-      v-bind="forwarded"
-      v-model="modelValue"
-      type="checkbox"
-      :class="checkboxClass"
-    />
-
-    <label v-if="!hasLabelSlot" :for="id">
-      {{ label }}
-    </label>
-    <label v-else-if="hasLabelSlot" :for="id">
-      <slot name="label" />
-    </label>
-
-    <svg
-      pointer-events="none"
-      width="16"
-      height="13"
-      viewBox="0 0 16 13"
-      class="absolute left-1 size-3.5 hidden group-has-checked:block"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M4.75025 10.0402L1.56941 6.8594C1.21191 6.5019 0.634414 6.5019 0.276914 6.8594C-0.0805859 7.2169 -0.0805859 7.7944 0.276914 8.1519L4.10858 11.9836C4.46608 12.3411 5.04358 12.3411 5.40108 11.9836L15.0994 2.28523C15.4569 1.92773 15.4569 1.35023 15.0994 0.992734C14.7419 0.635234 14.1644 0.635234 13.8069 0.992734L4.75025 10.0402Z"
-        fill="white"
+    <div class="group relative select-none">
+      <input
+        v-bind="forwarded"
+        v-model="modelValue"
+        type="checkbox"
+        :class="checkboxClass"
+        :disabled="props.disabled"
       />
-    </svg>
+
+      <GenericIcon
+        name="check"
+        class="pointer-events-none absolute top-[-1px] left-[-2px] hidden group-has-checked:block leading-none text-2xl text-white"
+        variant="rounded"
+      />
+    </div>
+
+    <slot>
+      <label v-if="label" :for="id">
+        {{ label }}
+      </label>
+    </slot>
   </div>
 </template>
